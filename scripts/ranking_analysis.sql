891fe2559dcde5d4e17234f51690a0c0c5c25524
Hajar Guemili <hajarguemili5@gmail.com>
2026-05-05 21:39:43 +0100

    Create ranking_analysis.sql for product revenue analysis
    Add SQL queries for ranking product performance based on revenue.

---
+++ scripts/ranking_analysis.sql
@@ -0,0 +1,33 @@
+--Ranking analysis
+--which 5 products generate the highest revenue?
+select  top 5 
+p.product_name,
+sum(f.sales_amount) as total_revenue
+from gold.fact_sales f
+left join gold.dim_products p
+on p.product_key=f.product_key
+group by p.product_name
+order by total_revenue desc 
+
+--what are the 5 worst-performing products in terms of sales?
+select  top 5 
+p.product_name,
+sum(f.sales_amount) as total_revenue
+from gold.fact_sales f
+left join gold.dim_products p
+on p.product_key=f.product_key
+group by p.product_name
+order by total_revenue 
+
+--ranking with window function
+select * from(
+select   
+p.product_name,
+sum(f.sales_amount) as total_revenue,
+row_number() over (order by sum(f.sales_amount) desc) as rank_products
+from gold.fact_sales f
+left join gold.dim_products p
+on p.product_key=f.product_key
+group by p.product_name)t
+where rank_products <=5 
+
